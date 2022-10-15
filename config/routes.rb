7999f7cd7c4b9f2ad01af8882d9ed4aa4f695928
Selvaraj <ksraj.odc@gmail.com>
2022-10-15 19:28:58 +0530

    Add course controller
    Update routes
    Add schema changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    namespace :api do
+      namespace :v1 do
+        resources :courses
+      end
+    end
+  end
 end
--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :course do
-    name Faker::Name.name
-    description Faker::Lorem.paragraph
+    name { Faker::Name.name }
+    description { Faker::Lorem.paragraph }
   end
 end
